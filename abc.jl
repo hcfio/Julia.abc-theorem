0fa08a75f5e1bb872579e780981459148ea113ae
Hiroyuki Chihara <77265300+hiroyuki-chihara@users.noreply.github.com>
2021-11-04 23:37:18 +0900

    Add files via upload

---
+++ abc.jl
@@ -0,0 +1,29 @@
+using CSV, Primes, DataFrames
+
+kappa=1.4;
+Q=[1 2 3];
+
+for a=1:1000
+    for b=a+1:100000
+        c=a+b;
+        X=factor(Vector,a*b*c);
+        L=length(X);
+        rad=X[1];
+        if [gcd(a,b) gcd(a,c) gcd(b,c)]==[1 1 1]
+            for i=2:L
+                if X[i]==X[i-1]
+                    rad=rad*1;
+                else
+                    rad=rad*X[i];
+                end
+            end
+            if (c >= rad^kappa)
+                q=[a b c];
+                Q=[Q;q];
+            end
+        end
+    end
+end
+
+R=DataFrame(Q,:auto);
+CSV.write("abc14.csv", R);
